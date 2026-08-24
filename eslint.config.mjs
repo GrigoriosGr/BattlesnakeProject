ad56a6bcde5d9935cfbb6a09250caa5e023b66c3
GrigoriosGR <grigorios.grigoropoulos@gmail.com>
2026-08-24 21:55:37 +0300

    Bug Fixes
    Bug Fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,25 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import sonarjs from "eslint-plugin-sonarjs";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import unicorn from "eslint-plugin-unicorn";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  ...sonarjs.configs.recommended,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      unicorn,
+    },
+    rules: {
+      "unicorn/prefer-module": "error",
+    },
+  },
 ]);
--- server.js
+++ server.js
@@ -30,10 +30,6 @@ export default function runServer(handlers) {
   const host = '0.0.0.0';
   const port = process.env.PORT || 8000;
 
-  app.listen(port, host, () => {
-    console.log(`Running Battlesnake at http://${host}:${port}...`)
-  });
-
   // Start the Express server
   app.listen(port, host, () => {
     console.log(`Running Battlesnake at http://${host}:${port}...`);
